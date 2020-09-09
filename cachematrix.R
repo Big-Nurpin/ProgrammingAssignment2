9dd1ee7dddc68f396c77b1011afab87e1d6933dc
Big-Nurpin <sdean.jeremy@gmail.com>
2020-09-09 12:40:10 -0700

    Added code for makeCacheMatrix function and edited the comments

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,24 @@
 ## of a matrix to save processing power on already
 ## calculated values
 
-## Computes and caches the inverse of a matrix in a matrix-like object
+## Caches a matrix in a matrix-like list
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    mat <- NULL                     #Initialize matrix variable
+    set <- function(y) {            #Set the value of the matrix
+        x <<- y
+        mat <-- NULL
+    }
+    get <- function() x                          #Get the value of the matrix
+    setinv <- function(inverse) mat <<- inverse  #Set value of inverse
+    getinv <- function() mat                     #Get value of inverse
+    list(set = set, get = get,                   #Return the list of values
+         setinv = setinv, getinv = getinv)
 }
 
 
-## Computes the above function, if already calculated, retrieves from cache
+## Computes the inverse above function, if already calculated, 
+## retrieves from cache
 
 cacheSolve <- function(x, ...) {
 
