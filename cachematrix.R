4434833646aba53bbd21c3b57339636e8d179fc8
Big-Nurpin <sdean.jeremy@gmail.com>
2020-09-09 12:16:19 -0700

    Added comments on functionality

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,15 @@
-## 
-## 
+## A pair of functions that compute and cache the inverse
+## of a matrix to save processing power on already
+# calculated values
 
-## 
+## Computes and caches the inverse of a matrix in a matrix-like object
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## 
+## Computes the above function, if already calculates, retrieves from cache
 
 cacheSolve <- function(x, ...) {
 
